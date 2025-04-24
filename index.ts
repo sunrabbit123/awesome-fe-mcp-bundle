1c3b5acf80ac4d2765d49796fe3a10ec7d756e06
sunrabbit123 <qudwls185@naver.com>
2025-04-24 23:00:08 +0900

    fix: not run server error
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- index.ts
+++ index.ts
@@ -4,54 +4,56 @@ const bundle = bundler({
   name: "awesome-fe-mcp-bundle",
   version: "0.0.1",
   mcpServers: {
-    "slack": {
+    calculate: {
       command: "npx",
-      args: [
-        "-y",
-        "@modelcontextprotocol/server-slack",
-      ],
+      args: ["-y", "@wrtnlabs/calculator-mcp"],
     },
-    "gitlab": {
-      command: "docker",
-      args: [
-        "run",
-        "--rm",
-        "-i",
-        "-e",
-        "GITLAB_PERSONAL_ACCESS_TOKEN",
-        "-e",
-        "GITLAB_API_URL",
-        "mcp/gitlab",
-      ],
-    },
-    "github": {
-      command: "npx",
-      args: [
-        "-y",
-        "@modelcontextprotocol/server-github",
-      ],
-    },
-    "mcp-atlassian": {
-      command: "uvx",
-      args: ["mcp-atlassian"],
-    },
+    // "slack": {
+    //   command: "npx",
+    //   args: [
+    //     "-y",
+    //     "@modelcontextprotocol/server-slack",
+    //   ],
+    // },
+    // "gitlab": {
+    //   command: "docker",
+    //   args: [
+    //     "run",
+    //     "--rm",
+    //     "-i",
+    //     "-e",
+    //     "GITLAB_PERSONAL_ACCESS_TOKEN",
+    //     "-e",
+    //     "GITLAB_API_URL",
+    //     "mcp/gitlab",
+    //   ],
+    // },
+    // "github": {
+    //   command: "npx",
+    //   args: [
+    //     "-y",
+    //     "@modelcontextprotocol/server-github",
+    //   ],
+    // },
+    // "mcp-atlassian": {
+    //   command: "uvx",
+    //   args: ["mcp-atlassian"],
+    // },
   },
   env: {
-    GITHUB_PERSONAL_ACCESS_TOKEN: "github",
-    SLACK_BOT_TOKEN: "slack",
-    SLACK_TEAM_ID: "slack",
-    GITLAB_PERSONAL_ACCESS_TOKEN: "gitlab",
-    GITLAB_API_URL: "gitlab",
-    CONFLUENCE_URL: "mcp-atlassian",
-    CONFLUENCE_USERNAME: "mcp-atlassian",
-    CONFLUENCE_API_TOKEN: "mcp-atlassian",
-    JIRA_URL: "mcp-atlassian",
-    JIRA_USERNAME: "mcp-atlassian",
-    JIRA_API_TOKEN: "mcp-atlassian",
+    // GITHUB_PERSONAL_ACCESS_TOKEN: "github",
+    // SLACK_BOT_TOKEN: "slack",
+    // SLACK_TEAM_ID: "slack",
+    // GITLAB_PERSONAL_ACCESS_TOKEN: "gitlab",
+    // GITLAB_API_URL: "gitlab",
+    // CONFLUENCE_URL: "mcp-atlassian",
+    // CONFLUENCE_USERNAME: "mcp-atlassian",
+    // CONFLUENCE_API_TOKEN: "mcp-atlassian",
+    // JIRA_URL: "mcp-atlassian",
+    // JIRA_USERNAME: "mcp-atlassian",
+    // JIRA_API_TOKEN: "mcp-atlassian",
   },
 });
 
-export default {
-  run: bundle.run,
-  createServer: bundle.createServer,
-};
+export const run = bundle.run;
+export const createServer = bundle.createServer;
